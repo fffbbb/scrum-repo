dbc1531ce8ba2ce30cb50bfc1e94a2531d8c2fd4
Ferhat Bahar <ferhat.bahar@sahibinden.com>
2015-09-28 17:25:01 +0300

    sprint2

---
+++ site.js
@@ -0,0 +1,27 @@
+$(function () {
+    var menu = [
+            {
+                text: 'Anasayfa',
+                url: '/'
+            },
+            {
+                text: 'Galeri',
+                url: '/galeri.html'
+            },
+            {
+                text: 'Davranışlar',
+                url: '/davranislar.html'
+            }
+        ],
+        i = 0;
+
+    for (; i < menu.length; i++) {
+        $('.menu').append(
+            '<li><a href="' + menu[i].url + '" > ' + menu[i].text + '</a></li>'
+
+        );
+    }
+
+
+
+});
