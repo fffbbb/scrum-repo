47069fe16c51fa676516cc9d77aed9b047566dfd
Ferhat Bahar <ferhat.bahar@sahibinden.com>
2015-09-29 12:42:11 +0300

    sprint-3

--- site.js
+++ site.js
@@ -38,3 +38,9 @@ $(function () {
 
 
 });
+
+
+function validateEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
